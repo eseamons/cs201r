e0e0e59f0ab70481222b5fc160fc9cc94285aad9
Eric Seamons <ericseamons90@gmail.com>
2015-09-22 21:57:14 +0000

    organizing folders

---
+++ lab_section_2_newsreader/app.js
@@ -0,0 +1,46 @@
+var main = function() {
+  $('.article').click(function() {
+    $('.article').removeClass('current');
+    $('.description').hide();
+
+    $(this).addClass('current');
+    $(this).children('.description').show();
+  });
+
+  $(document).keypress(function(event) {
+    if(event.which === 111) {
+    //   $('.description').hide();
+    //made improvments so it can toggle
+      $('.current').children('.description').toggle();
+    }
+
+    else if(event.which === 110) {
+      var currentArticle = $('.current');
+      
+      
+      
+      //will not change current article with 'n' key if focus on textfield for city
+      //added feature
+      if(!$('#cityfield').is(':focus')) {
+          //improved code to loop back around to beginning
+          var nextArticle = '';
+          
+          if(currentArticle.attr("id") != $('#last').attr("id")) {
+            nextArticle = currentArticle.next();
+          }
+          else {
+            nextArticle = $('#first');
+          }
+          
+          currentArticle.removeClass('current');
+          nextArticle.addClass('current');
+      }
+      
+      
+      
+      
+    }
+  });
+}
+
+$(document).ready(main);
