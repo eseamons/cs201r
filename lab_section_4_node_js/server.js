cc73398ec391ca4f18bd00ab792770473892407d
Eric Seamons <ericseamons90@gmail.com>
2015-10-13 22:16:06 +0000

    got the node.js server working for project 4. Also got more exercises done for in learnyounode

--- lab_section_4_node_js/server.js
+++ lab_section_4_node_js/server.js
@@ -1,36 +1,44 @@
 var fs = require('fs');
 var http = require('http');
 var url = require('url');
-var ROOT_DIR = "html/";
+var ROOT_DIR = "html";
+
+console.log("Server Running...");
+
 http.createServer(function (req, res) {
   var urlObj = url.parse(req.url, true, false);
-  fs.readFile(ROOT_DIR + urlObj.pathname, function (err,data) {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
-}).listen(8080);
-
-
-
-var options = {
-    hostname: 'localhost',
-    port: '8080',
-    path: '/hello.html'
-  };
-function handleResponse(response) {
-  var serverData = '';
-  response.on('data', function (chunk) {
-    serverData += chunk;
-  });
-  response.on('end', function () {
-    console.log(serverData);
-  });
-}
-http.request(options, function(response){
-  handleResponse(response);
-}).end();
+  console.log('opening '+ROOT_DIR+urlObj.pathname);
+  if(urlObj.pathname == "/getcity") {
+        //console.log("IN REST SERVICE");
+        res.writeHead(200);
+        //var cities = [{city:'Provo'},{city:'Price'},{city:'Alpine'}];
+        var myReg = new RegExp("^"+urlObj.query['q']);
+        fs.readFile('cities.dat.txt',function(err,data) {
+            if(err) {throw err;}
+            var cities = data.toString().split('\n');
+            var jsonresult = [];
+            for(i = 0; i < cities.length; i++) {
+                //console.log(cities[i]);
+                var result = cities[i].search(myReg);
+                if(result !== -1 && urlObj.query['q'] !== '') {
+                    jsonresult.push({city:cities[i]});
+                }
+            }
+            res.end(JSON.stringify(jsonresult));    
+        });
+        
+  }
+  else {
+      fs.readFile(ROOT_DIR + urlObj.pathname, function (err,data) {
+        if (err) {
+          res.writeHead(404);
+          res.end(JSON.stringify(err));
+          return;
+        }
+        res.writeHead(200);
+        res.end(data);
+      });
+  }
+  
+  
+}).listen(8124);
