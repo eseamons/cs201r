4aa3d5935676d4ac98d6b8b3c9e6a73d0492848d
Eric Seamons <ericseamons90@gmail.com>
2015-11-03 20:40:10 +0000

    finished mongodb pair programming project

--- mongodb_pair_project/server.js
+++ mongodb_pair_project/server.js
@@ -1,58 +1,3 @@
-/**
- * This array of pokemon will represent a piece of data in our 'database'
- */
-var students = [
-  {
-    name: 'Tommy',
-    gender: 'M',
-    gpa: 3.67,
-    major: 'Business',
-    class_standing: 'freshman',
-    age: 18,
-  },
-  {
-    name: 'Sally',
-    gender: 'F',
-    gpa: 3.79,
-    major: 'Computer Science',
-    class_standing: 'junior',
-    age: 23,
-
-  },
-  {
-    name: 'Matt',
-    gender: 'M',
-    gpa: 3.31,
-    major: 'Statistics',
-    class_standing: 'senior',
-    age: 25,
-  },
-  {
-    name: 'Peter',
-    gender: 'M',
-    gpa: 3.97,
-    major: 'Asian Studies',
-    class_standing: 'sophomore',
-    age: 22,
-  },
-  {
-    name: 'Claire',
-    gender: 'F',
-    gpa: 3.84,
-    major: 'Marriage and Family Sciences',
-    class_standing: 'sophomore',
-    age: 21,
-  },
-  {
-    name: 'Adam',
-    gender: 'M',
-    gpa: 4.0,
-    major: 'Computer Science',
-    class_standing: 'junior',
-    age: 23,
-  }
-];
-
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
@@ -63,23 +8,132 @@ http.createServer(function (req, res) {
   var urlObj = url.parse(req.url, true, false);
 
   if (urlObj.pathname === '/report') {
-      
-     var jsonresult = [];
-     /**
-     * TODO: return the array of pokemon above as a string
-     * with an header status of 'ok'
-     */
-    students.forEach(function(student) {
-        //console.log(poke.name);
-        jsonresult.push({name:student.name, gender:student.gender, major:student.major, class_standing: student.class_standing,gpa:student.gpa, age: student.age});
-    });
-      
-      
-    res.writeHead(200);
-     
-    res.end(JSON.stringify(jsonresult));
 
-  } else {
+	var MongoClient = require('mongodb').MongoClient;
+	      MongoClient.connect("mongodb://localhost/academic", function(err, db) {
+	        if(err) throw err;
+	        db.collection("students", function(err, comments){
+	          if(err) throw err;
+	          comments.find(function(err, items){
+       	     items.toArray(function(err, itemArr){
+       	       console.log("Document Array: ");
+       	       console.log(itemArr);
+       	       res.writeHead(200);
+       	       res.end(JSON.stringify(itemArr));
+       	     });
+       	   });
+       	  });
+     	 });
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+  else if(urlObj.pathname === '/report-add') {
+
+
+      var jsonData = "";
+      req.on('data', function (chunk) {
+        jsonData += chunk;
+      });
+      req.on('end', function () {
+        var reqObj = JSON.parse(jsonData);
+        console.log(reqObj);
+        // Now put it into the database
+        var MongoClient = require('mongodb').MongoClient;
+        MongoClient.connect("mongodb://localhost/academic", function(err, db) {
+          if(err) throw err;
+	  var insertObj = [{name:reqObj.studName,age:reqObj.studAge,gpa:reqObj.studGpa,gender:reqObj.studGender,major:reqObj.studMajor,class_standing:reqObj.studClassStanding}]
+          db.collection('students').insert(insertObj,function(err, records) {
+            console.log("Record added as "+records[0]._id);
+	    res.writeHead(200);
+            res.end("");
+          });
+        });
+      });   
+
+
+
+
+  }
+  else if(urlObj.pathname === '/report-delete'){
+       var jsonData = "";
+
+       req.on('data', function (chunk) {
+          jsonData += chunk;
+       });
+       req.on('end', function () {
+        var reqObj = JSON.parse(jsonData);
+        console.log(reqObj._id);
+        console.log("delete student");
+
+// Now delete from database	
+        var MongoClient = require('mongodb').MongoClient;
+        var ObjectId = require('mongodb').ObjectID;
+        MongoClient.connect("mongodb://localhost/academic", function(err, db) {
+          if(err) throw err;
+          db.collection('students').remove({"_id":ObjectId(reqObj._id)},function(err, records) {
+            console.log("Deleted student");
+	    res.writeHead(200);
+            res.end("");
+          });
+        });
+
+
+
+
+
+
+       }); //end of req on end
+       
+
+
+
+  }
+  else if(urlObj.pathname === '/report-update') {
+               var jsonData = "";
+
+       req.on('data', function (chunk) {
+          jsonData += chunk;
+       });
+       req.on('end', function () {
+        var reqObj = JSON.parse(jsonData);
+        console.log(reqObj);
+        console.log("update student");
+     
+
+
+
+      // Now update the database
+	var insertObj = {name:reqObj.studName,age:reqObj.studAge,gpa:reqObj.studGpa,gender:reqObj.studGender,major:reqObj.studMajor,class_standing:reqObj.studClassStanding};
+        var MongoClient = require('mongodb').MongoClient;
+        var ObjectId = require('mongodb').ObjectID;
+        MongoClient.connect("mongodb://localhost/academic", function(err, db) {
+          if(err) throw err;
+          console.log(insertObj);
+	  console.log(reqObj._id);
+          db.collection('students').update({"_id" : ObjectId(reqObj._id)},{ $set:{"name":insertObj.name,"age":insertObj.age,"gpa":insertObj.gpa,"gender":insertObj.gender,"major":insertObj.major,"class_standing":insertObj.class_standing}}, {upsert: true},function(err, results) {
+	    res.writeHead(200);
+            res.end("");
+          });
+
+
+
+        });
+
+       });
+
+
+  }
+  else {
 
     /**
      * Here is where we return all requests for files in our 'src' directory
--- mongodb_pair_project/src/app.js
+++ mongodb_pair_project/src/app.js
@@ -18,7 +18,7 @@ function reportFetcher ($http) {
 
 }
 
-function mainCtrl ($scope, reportFetcher) {
+function mainCtrl ($scope,$http, reportFetcher) {
 
   $scope.students = [];
   $scope.show = false;
@@ -34,6 +34,13 @@ function mainCtrl ($scope, reportFetcher) {
   $scope.data.studGpa = '';
   $scope.data.studMajor = '';
   $scope.data.studClassStanding = '';
+  $scope.data.options =
+     [
+       {value: 'Freshman', name: 'Freshman'},
+       {value: 'Sophomore', name: 'Sophomore'},
+       {value: 'Junior', name: 'Junior'},
+       {value: 'Senior', name: 'Senior'}
+     ];
   
     $scope.view = function(){
         reportFetcher.get()
@@ -48,14 +55,40 @@ function mainCtrl ($scope, reportFetcher) {
     $scope.add_student = function(){
         editing = -1;
         $('.modal-title').text('Add New Student');
+	$scope.data.studName = '';
+         $scope.data.studAge = '';
+         $scope.data.studGender = '';
+         $scope.data.studGpa = '';
+         $scope.data.studMajor = '';
+         $scope.data.studClassStanding = '';
         $scope.showModal = true;
         
     };
     
-    
+    $scope.delete_student = function(stud) {
+                                   
+    var post_data = JSON.stringify($scope.students[stud]);
+	   var res = $http.post('/report-delete', post_data);
+           res.success(function() {
+              reportFetcher.get()
+        .then(function (data) {
+          $scope.students = data;
+          $scope.show = true;
+        });
+
+
+
+       });
+
+
+
+    };  
+
+
     $scope.edit_record = function(stud) {
         editing = stud;
         var student = $scope.students[stud];
+	$scope.data._id = student._id;
         $scope.data.studName = student.name;
         $scope.data.studAge = student.age;
         $scope.data.studGender = student.gender;
@@ -65,25 +98,49 @@ function mainCtrl ($scope, reportFetcher) {
         $('.modal-title').text('Edit Record for '+ student.name);
         $scope.showModal = true;
     };
-    
-    
     $scope.submitStudent = function() {
         $scope.showModal = false;
         
         
         
         if(editing == -1) {
-            $scope.students.push({name:$scope.data.studName, age:$scope.data.studAge, gender:$scope.data.studGender, gpa:$scope.data.studGpa, major:$scope.data.studMajor,class_standing:$scope.data.studClassStanding});
-        }
+	   var post_data = JSON.stringify($scope.data);
+	   var res = $http.post('/report-add', post_data);
+           res.success(function() {
+              reportFetcher.get()
+        .then(function (data) {
+          $scope.students = data;
+          $scope.show = true;
+        });
+
+
+
+       });
+
+
+
+      	}
         else {
+
+
+               var post_update = JSON.stringify($scope.data);
+	   var res = $http.post('/report-update', post_update);
+           res.success(function() {
+              reportFetcher.get()
+        .then(function (data) {
+          $scope.students = data;
+          $scope.show = true;
+        });
+
+       });
+/*
            $scope.students[editing].name = $scope.data.studName;
            $scope.students[editing].age = $scope.data.studAge;
-           
            $scope.students[editing].gender = $scope.data.studGender;
            $scope.students[editing].major = $scope.data.studMajor;
            $scope.students[editing].gpa = $scope.data.studGpa;
            $scope.students[editing].class_standing = $scope.data.studClassStanding;
-           
+ */        
         }
         
         
